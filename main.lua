92c6779d41326fdd0d67c308e5956967a9e38e6e
coyote2_4 <90734529+swaggedoutindividual@users.noreply.github.com>
2026-09-02 19:57:30 +1000

    Create main.lua
    made it sexy

---
+++ main.lua
@@ -0,0 +1,361 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#=                =#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@%-                          -%@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@#                                  #@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@* :=%@@@@@%-                                      -%@@@@@%=: *@@@@@@@@@
+@@@@@@@@@@@=      +%@#..                                 .=@%+      -@@@@@@@@@@@
+@@@@@@@@@@@@%          .=*@++   .+#@#======#@#+.   -+%#=.          %@@@@@@@@@@@@
+@@@@@@@@@@@@@@@.              -%@@.:@@@@@@@@-.@@@-              .@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@=.%+==%@*+-          =%#@@@@@@%%=          -+*@%==+@-=@@@@@@@@@@@@@
+@@@@@@@@@@@@+   :@                 :@@@@@@@@:                 @*   :@@@@@@@@@@@@
+@@@@@@@@@@@+      #=                +@@@@@@*                -%      =@@@@@@@@@@@
+@@@@@@@@@@@        :@@@@%%%*.        =@@@@+         +%%%@@@@=        %@@@@@@@@@@
+@@@@@@@@@@:         .@*              :*  *-              *@.         :@@@@@@@@@@
+@@@@@@@@@@          @@@@:             -%%-             .@@@%          @@@@@@@@@@
+@@@@@@@@@#         -@@@@@@@@@@@@@@=    .:    -@@@@@@@@@@@@@@:         -@@@@@@@@@
+@@@@@@@@@.         -@@@@@@@@@@@@@=            =@@: :%@@@@@@@-          @@@@@@@@@
+@@@@@@@@@.         -@@@@@@%-@@@@.     +@@+     .@@@* *@@@@@@-          @@@@@@@@@
+@@@@@@@@@-         -@@@@@@# @@@@-.=@@@@@@@@@@=.:@@@@: -@@@@@=         .@@@@@@@@@
+@@@@@@@@@@          @@-::.= +-=:@ :++++++++++: %:=-     *@@@          %@@@@@@@@@
+@@@@@@@@@@. :. #@%# -@#  *# @%*..::*@@@@@@@@*.: .*##   *@@@. %% -#*   @@@@@@@@@@
+@@@@@@@@@@= %%:@:-%  #@@@@#:@@@@@@@@@@@@@@@@@@@@@@@@. @@@@* .@%@# @- -@@@@@@@@@@
+@@@@@@@@@@@- --:  .@: *@@@# @@@@@@@@@@@@@@@@@@@@@#%.=@@@@+ **    ++ :@@@@@@@@@@@
+@@@@@@@@@@@@     :%@@- -@@@@@@@@@@@@@@@@@@@@@@@@# :%@@@@: . @= ...  @@@@@@@@@@@@
+@@@@@@@@@@@@%  %@=.  .   -@@@@@@@@@@@@@@@@@@@@@@@@@@@@-  .#@@*---  %@@@@@@@@@@@@
+@@@@@@@@@@@@@@-    =@%%%@: :%@@@@@@@@@@@@@@@@@@@@@@%. -@%%%  %@* :@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@*   @+   @@ +  :+@@@@@@@@@@@@@@@@=.   :@#=@%+.   +@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@. -@@@@@:*%#@*        ::        @@%@@:.@*    .@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@-     %#@*%%.@-   # += -# +% #*   =@. -. -@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@%  :@ *@  @@@ #@# .@=@@@+* .@@-%@*   +@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@+. .+ +% #@=%+  :@# =@+       .=@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@%.      %=   +-        %@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*========*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ ░▒▓███████▓▒░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░░▒▓██████████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ 
+░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░         ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+ ░▒▓██████▓▒░   ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░  
+       ░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
+       ░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░ 
+░▒▓███████▓▒░   ░▒▓█▓▒░   ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█████████████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
+                                                                                                                                                 
+                                                                                                                                                 
+ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓██████████████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓████████▓▒░▒▓████████▓▒░              
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░                  
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░                  
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░    ░▒▓█▓▒░                  
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░                  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░                  
+ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░          ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓█▓▒░   
+--STORMWORKS COMBAT NET
+
+--BY coyote2_4
+
+-- 2 FRIENDLIES + 5 OPFOR
+
+-- NUMBER INPUTS
+--  1 = Friendly 1 X
+--  2 = Friendly 1 Y
+--  3 = Friendly 1 Altitude
+--  4 = Friendly 2 X
+--  5 = Friendly 2 Y
+--  6 = Friendly 2 Altitude
+
+
+--  7 = OPFOR 1 X
+--  8 = OPFOR 1 Y
+--  9 = OPFOR 1 Altitude
+-- 10 = OPFOR 2 X
+-- 11 = OPFOR 2 Y
+-- 12 = OPFOR 2 Altitude
+-- 13 = OPFOR 3 X
+-- 14 = OPFOR 3 Y
+-- 15 = OPFOR 3 Altitude
+-- 16 = OPFOR 4 X
+-- 17 = OPFOR 4 Y
+-- 18 = OPFOR 4 Altitude
+-- 19 = OPFOR 5 X
+-- 20 = OPFOR 5 Y
+-- 21 = OPFOR 5 Altitude
+-- 22 = YOUR VEHICLE X
+-- 23 = YOUR VEHICLE Y
+-- 24 = MAP ZOOM
+
+-- BOOLEAN INPUT
+
+-- Bool 1 = Dark Mode
+
+
+
+function onTick()
+
+
+    -- FRIENDLIES
+
+
+    friendly1X = input.getNumber(1)
+    friendly1Y = input.getNumber(2)
+    friendly1Alt = input.getNumber(3)
+
+    friendly2X = input.getNumber(4)
+    friendly2Y = input.getNumber(5)
+    friendly2Alt = input.getNumber(6)
+
+
+    -- OPFOR
+
+
+    opfor1X = input.getNumber(7)
+    opfor1Y = input.getNumber(8)
+    opfor1Alt = input.getNumber(9)
+
+    opfor2X = input.getNumber(10)
+    opfor2Y = input.getNumber(11)
+    opfor2Alt = input.getNumber(12)
+
+    opfor3X = input.getNumber(13)
+    opfor3Y = input.getNumber(14)
+    opfor3Alt = input.getNumber(15)
+
+    opfor4X = input.getNumber(16)
+    opfor4Y = input.getNumber(17)
+    opfor4Alt = input.getNumber(18)
+
+    opfor5X = input.getNumber(19)
+    opfor5Y = input.getNumber(20)
+    opfor5Alt = input.getNumber(21)
+
+
+    --====================================================
+    -- YOUR VEHICLE
+    --====================================================
+
+    vehX = input.getNumber(22)
+    vehY = input.getNumber(23)
+    zoom = input.getNumber(24)
+
+    darkMode = input.getBool(1)
+
+end
+
+
+function drawTarget(x, y, label, targetType, altitude)
+
+    -- Convert GPS position to screen position
+    local sx, sy = map.mapToScreen(
+        vehX,
+        vehY,
+        zoom,
+        w,
+        h,
+        x,
+        y
+    )
+
+  
+    -- FRIENDLY
+
+
+    if targetType == 1 then
+
+        screen.setColor(40, 140, 255, 255)
+
+        -- Diamond
+        screen.drawLine(sx, sy - 4, sx + 4, sy)
+        screen.drawLine(sx + 4, sy, sx, sy + 4)
+        screen.drawLine(sx, sy + 4, sx - 4, sy)
+        screen.drawLine(sx - 4, sy, sx, sy - 4)
+
+    -- OPFOR
+  
+
+    else
+
+        screen.setColor(230, 45, 45, 255)
+
+        -- Diamond
+        screen.drawLine(sx, sy - 4, sx + 4, sy)
+        screen.drawLine(sx + 4, sy, sx, sy + 4)
+        screen.drawLine(sx, sy + 4, sx - 4, sy)
+        screen.drawLine(sx - 4, sy, sx, sy - 4)
+
+    end
+
+
+    
+    -- LABEL
+
+
+    screen.setColor(255, 255, 255, 255)
+
+    screen.drawText(
+        sx + 6,
+        sy - 6,
+        label
+    )
+
+
+    -- ALTITUDE
+
+
+    screen.setColor(190, 190, 190, 255)
+
+    screen.drawText(
+        sx + 6,
+        sy + 3,
+        math.floor(altitude) .. "m"
+    )
+
+end
+
+
+function onDraw()
+  local w = screen.getWidth()
+  local h = screen.getHeight()
+
+    -- MAP COLOURS
+
+
+    if darkMode then
+
+        -- DARK COLOUR SCHEME
+
+        screen.setMapColorOcean(25, 55, 70, 255)
+        screen.setMapColorShallows(55, 90, 105, 255)
+        screen.setMapColorGrass(50, 70, 45, 255)
+        screen.setMapColorGravel(75, 75, 75, 255)
+        screen.setMapColorLand(75, 60, 45, 255)
+        screen.setMapColorRock(30, 30, 30, 255)
+        screen.setMapColorSand(120, 105, 75, 255)
+        screen.setMapColorSnow(130, 135, 135, 255)
+
+    else
+
+        -- NORMAL COLOUR SCHEME
+
+        screen.setMapColorOcean(25, 110, 170, 255)
+        screen.setMapColorShallows(70, 150, 185, 255)
+        screen.setMapColorGrass(75, 125, 55, 255)
+        screen.setMapColorGravel(140, 140, 140, 255)
+        screen.setMapColorLand(125, 105, 75, 255)
+        screen.setMapColorRock(85, 85, 85, 255)
+        screen.setMapColorSand(215, 190, 120, 255)
+        screen.setMapColorSnow(220, 220, 220, 255)
+
+    end
+
+
+
+    -- DRAW MAP
+
+
+    screen.drawMap(vehX, vehY, zoom)
+
+
+    
+    -- DRAW FRIENDLIES
+   
+
+    -- A coordinate of 0,0 is treated as no target
+    if friendly1X ~= 0 or friendly1Y ~= 0 then
+        drawTarget(
+            friendly1X,
+            friendly1Y,
+            "F1",
+            1,
+            friendly1Alt
+        )
+    end
+
+
+    if friendly2X ~= 0 or friendly2Y ~= 0 then
+        drawTarget(
+            friendly2X,
+            friendly2Y,
+            "F2",
+            1,
+            friendly2Alt
+        )
+    end
+
+
+
+    -- DRAW OPFOR
+
+
+    if opfor1X ~= 0 or opfor1Y ~= 0 then
+        drawTarget(
+            opfor1X,
+            opfor1Y,
+            "O1",
+            0,
+            opfor1Alt
+        )
+    end
+
+
+    if opfor2X ~= 0 or opfor2Y ~= 0 then
+        drawTarget(
+            opfor2X,
+            opfor2Y,
+            "O2",
+            0,
+            opfor2Alt
+        )
+    end
+
+
+    if opfor3X ~= 0 or opfor3Y ~= 0 then
+        drawTarget(
+            opfor3X,
+            opfor3Y,
+            "O3",
+            0,
+            opfor3Alt
+        )
+    end
+
+
+    if opfor4X ~= 0 or opfor4Y ~= 0 then
+        drawTarget(
+            opfor4X,
+            opfor4Y,
+            "O4",
+            0,
+            opfor4Alt
+        )
+    end
+
+
+    if opfor5X ~= 0 or opfor5Y ~= 0 then
+        drawTarget(
+            opfor5X,
+            opfor5Y,
+            "O5",
+            0,
+            opfor5Alt
+        )
+    end
+
+
+    --====================================================
+    -- YOUR VEHICLE
+    --====================================================
+
+    screen.setColor(50, 255, 80, 255)
+
+    -- Your vehicle marker
+    screen.drawCircleF(w / 2, h / 2, 3)
+
+    screen.setColor(255, 255, 255, 255)
+
+    screen.drawText(
+        w / 2 + 6,
+        h / 2 - 6,
+        "YOU"
+    )
+
+end
+
